8ad8b82ad041cfaed11c47bb20d75c0d2d36f783
Stefano Peluchetti <phd.st.p@gmail.com>
2015-11-20 19:03:26 +0000

    Prepare for release v1.0.0-beta10.

---
+++ __meta.lua
@@ -0,0 +1,12 @@
+
+return {
+  name = "sci-lang",
+  version = "1.0.beta10",
+  require = {
+    luajit = "2.0",
+    sci = "1.0.beta10",
+  },
+  homepage = "https://github.com/stepelu/lua-sci-lang",
+  license  = "MIT/X11",
+  description = "Syntax extensions to LuaJIT for scientific computing",
+}
